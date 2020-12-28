321cb76fa6d3a35c6d591391bb2cdccea97ead31
John Lins <46113320+JohnLins@users.noreply.github.com>
2020-12-27 20:24:22 -0800

    Update gensimBasicWord2Vec.py

--- Word2Vec/gensimBasicWord2Vec.py
+++ Word2Vec/gensimBasicWord2Vec.py
@@ -1,24 +1,23 @@
 #credit: https://machinelearningmastery.com/develop-word-embeddings-python-gensim/
 
 from gensim.models import Word2Vec
-# define training data
 sentences = [['this', 'is', 'the', 'first', 'sentence', 'for', 'word2vec'],
-			['this', 'is', 'the', 'second', 'sentence'],
-			['yet', 'another', 'sentence'],
-			['one', 'more', 'sentence'],
-			['and', 'the', 'final', 'sentence']]
-# train model
+	['this', 'is', 'the', 'second', 'sentence'],
+	['yet', 'another', 'sentence'],
+	['one', 'more', 'sentence'],
+	['and', 'the', 'final', 'sentence']]
+
 model = Word2Vec(sentences, min_count=1)
-# summarize the loaded model
+
 print(model)
-# summarize vocabulary
+
 words = list(model.wv.vocab)
 print(words)
-# access vector for one word
+
 print("sentence:", model['sentence'])
-# save model
+
 model.save('model.bin')
-# load model
+
 new_model = Word2Vec.load('model.bin')
 print(new_model)
 
